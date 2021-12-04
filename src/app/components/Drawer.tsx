38b5d8dd74dd0d1a34436692ec5f88f58c552434
Danail Iliev <danail.iliev99@gmail.com>
2021-12-04 21:38:06 +0200

    Drawer Icons

--- src/app/components/Drawer.tsx
+++ src/app/components/Drawer.tsx
@@ -1,8 +1,13 @@
-import { Drawer, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import { Drawer, List, ListItem, ListItemText, IconButton,} from "@mui/material";
 import { createStyles, makeStyles } from "@mui/styles";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import FeedIcon from '@mui/icons-material/Feed';
+import LoginIcon from '@mui/icons-material/Login';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
 
 const useStyles = makeStyles(() =>
     createStyles({
@@ -33,41 +38,45 @@ export const DrawerComponent: React.FunctionComponent = () => {
         setOpenDrawer(false)
     }
 
-
     return (
         <Fragment >
             <Drawer
                 open={openDrawer}
                 onClose={CloseDrawer}
                 // className={classes.draw}
-                style={{
-                    zIndex:1099,
-                }}
+                // style={{
+                //     zIndex:1101,
+                // }}
                 anchor="right"
             >
                 <List>
                     <ListItem onClick={CloseDrawer}>
                         <ListItemText>
+                            <HomeIcon/>
                             <Link className={classes.link} to="/">Home</Link>
                         </ListItemText>
                     </ListItem>
                     <ListItem onClick={CloseDrawer} className={classes.about}>
                         <ListItemText>
+                            <FeedIcon/>
                             <Link className={classes.link} to="/about" >About</Link>
                         </ListItemText>
                     </ListItem>
                     <ListItem onClick={CloseDrawer}>
                         <ListItemText>
+                            <LoginIcon/>
                             <Link className={classes.link} to="/login">Login</Link>
                         </ListItemText>
                     </ListItem>
                     <ListItem onClick={CloseDrawer}>
                         <ListItemText>
+                            <ContactMailIcon/>
                             <Link className={classes.link} to="/contactsUs">Contact us</Link>
                         </ListItemText>
                     </ListItem>
                     <ListItem onClick={CloseDrawer}>
                         <ListItemText>
+                            <ConfirmationNumberIcon/>
                             <Link className={classes.link} to="/tickets">Tickets</Link>
                         </ListItemText>
                     </ListItem>
